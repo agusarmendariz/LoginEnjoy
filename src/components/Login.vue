3af943368e49019f7eba287ae563de2f6f0b95f0
agusarmendariz <armendarizagustina.m@gmail.com>
2022-07-19 19:38:28 -0300

    login changed

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,19 +2,20 @@
   <div>
     <div id="Login"></div>
 
-    <section class="vh-100 gradient-custom">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+    <section class="vh-110 gradient-custom">
+      <div class="container py-5 h-80">
+        <div class="row d-flex justify-content-center align-items-center h-90">
+          <div class="col-10 col-lg-8 col-md-6 col-xl-5">
             <div class="card bg-dark text-white" style="border-radius: 1rem">
               <div class="card-body p-5 text-center">
-                <div class="mb-md-5 mt-md-4 pb-5">
-                  <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                  <p class="text-white-50 mb-5">
-                    Ingrese su usuario y contraseña
+                <div class="mb-md-8 mt-md-5 pb-2">
+                  <h2 class="fw-bold mb-2 text-uppercase"> enjoy Casino</h2>
+                  <br>
+                  <p class="text-white-50 mb-7">
+                    Bienvenido! Por favor, ingrese su usuario y contraseña
                   </p>
-
-                  <div class="form-outline form-white mb-4">
+                  <br>
+                  <div class="form-outline form-white mb-2">
                     <input
                       type="email"
                       id="typeEmailX"
@@ -22,8 +23,8 @@
                     />
                     <label class="form-label" for="typeEmailX">Email</label>
                   </div>
-
-                  <div class="form-outline form-white mb-4">
+                  <br>
+                  <div class="form-outline form-white mb-2">
                     <input
                       type="password"
                       id="typePasswordX"
@@ -33,38 +34,16 @@
                       >Contraseña</label
                     >
                   </div>
-
-                  <p class="small mb-5 pb-lg-2">
-                    <a class="text-white-50" href="#!">Olvidó su contraseña?</a>
-                  </p>
-
+                  
+                  <br>
                   <button
-                    class="btn btn-outline-light btn-lg px-5"
+                    class="btn btn-outline-light btn-lg px-5
+                    "
                     type="submit"
                   >
                     Login
                   </button>
 
-                  <div
-                    class="d-flex justify-content-center text-center mt-4 pt-1"
-                  >
-                    <a href="#!" class="text-white"
-                      ><i class="fab fa-facebook-f fa-lg"></i
-                    ></a>
-                    <a href="#!" class="text-white"
-                      ><i class="fab fa-twitter fa-lg mx-4 px-2"></i
-                    ></a>
-                    <a href="#!" class="text-white"
-                      ><i class="fab fa-google fa-lg"></i
-                    ></a>
-                  </div>
-                </div>
-
-                <div>
-                  <p class="mb-0">
-                    No tiene cuenat?
-                    <a href="#!" class="text-white-50 fw-bold">Registrarse</a>
-                  </p>
                 </div>
               </div>
             </div>
@@ -81,11 +60,13 @@ export default {
     return;
   },
 };
+
+
 </script>
- <style>
 
+<style>
 
- .gradient-custom {
+.gradient-custom {
     /* fallback for old browsers */
     background: #6a11cb;
     
@@ -95,4 +76,7 @@ export default {
     /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
     }
- </style>
+
+</style>
+
+
